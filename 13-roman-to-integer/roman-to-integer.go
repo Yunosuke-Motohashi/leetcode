6f0e71456753e36d0f3da6abf220c2c84d3a358b
Yunosuke Motohashi <72207423+Yunosuke-Motohashi@users.noreply.github.com>
2025-05-18 10:50:52 +0900

    Time: 2 ms (49.27%) | Memory: 4.9 MB (20.52%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.go
+++ 13-roman-to-integer/roman-to-integer.go
@@ -2,14 +2,14 @@
 func romanToInt(s string) int {
 
 	romanMap := map[byte]int{
-		'I': 1,
-		'V': 5,
-		'X': 10,
-		'L': 50,
-		'C': 100,
-		'D': 500,
-		'M': 1000,
-	}
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
 
 	result := 0
 	preVal := 0
