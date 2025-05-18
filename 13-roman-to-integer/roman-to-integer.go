5e194bc9d149916ee8cb7a584727d7279d1bba91
Yunosuke Motohashi <72207423+Yunosuke-Motohashi@users.noreply.github.com>
2025-05-18 10:44:13 +0900

    Time: 50 ms (5.47%) | Memory: 8 MB (5.98%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.go
+++ 13-roman-to-integer/roman-to-integer.go
@@ -3,34 +3,33 @@ import "strings"
 
 func romanToInt(s string) int {
 
-	romanMap := map[string]int{
-		"I": 1,
-		"V": 5,
-		"X": 10,
-		"L": 50,
-		"C": 100,
-		"D": 500,
-		"M": 1000,
+	romanMap := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
 	}
-    if len(s) == 1 {
-        return romanMap[s]
-    }
-    result := 0
-    preVal := 0
-    Val := 0
+	if len(s) == 1 {
+		return romanMap[s[0]]
+	}
+	result := 0
+	preVal := 0
+	Val := 0
 
-    for i := len(s)-1; i>=0; i-- {
-        key := string(s[i])
-        Val, _ = romanMap[string(key)]
-        fmt.Println(result, preVal, Val)
-        if Val >= preVal{
-            result += Val
-        } else {
-            result -= Val
-        }
-        
-        preVal = Val
-    }
+	for i := len(s) - 1; i >= 0; i-- {
+		Val, _ = romanMap[s[i]]
+		fmt.Println(result, preVal, Val)
+		if Val >= preVal {
+			result += Val
+		} else {
+			result -= Val
+		}
+
+		preVal = Val
+	}
 
 	fmt.Println(romanMap)
 	return result
