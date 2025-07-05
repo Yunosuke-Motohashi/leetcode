8acf58391f803bf83aca431199226a7ef0b7f3af
Yunosuke Motohashi <72207423+Yunosuke-Motohashi@users.noreply.github.com>
2025-07-06 00:44:51 +0900

    Time: 0 ms (100.00%) | Memory: 3.9 MB (17.72%) - LeetSync

--- 1086-divisor-game/divisor-game.go
+++ 1086-divisor-game/divisor-game.go
@@ -1,18 +1,34 @@
 // import "fmt"
 func divisorGame(n int) bool {
-    count := 0
-    for n > 1 {
-        x := 1
-        if n % x == 0 {
-            n = n-x
-            // fmt.Println(n, count)
+    dp := make([]bool, 1001)
+    dp[0], dp[1] = false, false
+    for i :=2; i<n+1; i++ {
+        for j:=1; j*j < i; j++ {
+            if i%j == 0 {
+                if !dp[i-j] {
+                    dp[i] = true
+                    break
+                }
+            }
         }
-        count += 1
+        // fmt.Println(dp[1:n+1])
     }
-    // fmt.Println(n, count)
-    if count % 2 == 0 {
-        return false
-    } else {
-        return true
-    }
+    return dp[n]
 }
+// func divisorGame(n int) bool {
+//     count := 0
+//     for n > 1 {
+//         x := 1
+//         if n % x == 0 {
+//             n = n-x
+//             // fmt.Println(n, count)
+//         }
+//         count += 1
+//     }
+//     // fmt.Println(n, count)
+//     if count % 2 == 0 {
+//         return false
+//     } else {
+//         return true
+//     }
+// }
