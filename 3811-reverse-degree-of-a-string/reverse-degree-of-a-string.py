cf1c7578f90f613d0825202f717f174ff9b5d2bc
Yunosuke Motohashi <72207423+Yunosuke-Motohashi@users.noreply.github.com>
2026-09-20 16:24:20 +0900

    Time: 7 ms (70.00%) | Memory: 19.2 MB (55.57%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -1,14 +1,30 @@
-import string
+# import string
+# class Solution:
+#     def reverseDegree(self, s: str) -> int:
+#         # print(string.ascii_lowercase)
+#         a_z_idx = {}
+#         for i, v in enumerate(string.ascii_lowercase):
+#             a_z_idx[v] = 26-i
+#         # print(a_z_idx)
+#         ans = 0
+#         for i, v in enumerate(s):
+#             # print(ans, i+1, a_z_idx[v])
+#             ans += (i+1) * a_z_idx[v]
+
+#         return ans
+
 class Solution:
     def reverseDegree(self, s: str) -> int:
         # print(string.ascii_lowercase)
-        a_z_idx = {}
-        for i, v in enumerate(string.ascii_lowercase):
-            a_z_idx[v] = 26-i
+        # a_z_idx = {}
+        # for i, v in enumerate(string.ascii_lowercase):
+        #     a_z_idx[v] = 26-i
         # print(a_z_idx)
         ans = 0
         for i, v in enumerate(s):
-            # print(ans, i+1, a_z_idx[v])
-            ans += (i+1) * a_z_idx[v]
+            s_value = ord("z") - ord(v) +1
+            ans += (i+1) * s_value
+            # print(ans, i+1, s_value)
+
 
         return ans
