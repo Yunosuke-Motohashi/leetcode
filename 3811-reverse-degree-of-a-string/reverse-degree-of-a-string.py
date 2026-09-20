e46a39fb0a7d8834b31479c81013ad941b4f392b
Yunosuke Motohashi <72207423+Yunosuke-Motohashi@users.noreply.github.com>
2026-09-20 16:20:41 +0900

    Time: 7 ms (70.00%) | Memory: 19.3 MB (55.57%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -0,0 +1,14 @@
+import string
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        # print(string.ascii_lowercase)
+        a_z_idx = {}
+        for i, v in enumerate(string.ascii_lowercase):
+            a_z_idx[v] = 26-i
+        # print(a_z_idx)
+        ans = 0
+        for i, v in enumerate(s):
+            # print(ans, i+1, a_z_idx[v])
+            ans += (i+1) * a_z_idx[v]
+
+        return ans
